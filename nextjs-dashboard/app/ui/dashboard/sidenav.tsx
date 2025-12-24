0ddd276bd0f2f126e6fce742da850f59b8d194a6
Brittani Lovelace <blovelace84@gmail.com>
2025-12-24 15:00:54 -0500

    Adding metadata

--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -3,6 +3,11 @@ import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
 import { signOut } from "@/auth";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "This is my Dashboard side Navigation",
+};
 
 export default function SideNav() {
   return (
